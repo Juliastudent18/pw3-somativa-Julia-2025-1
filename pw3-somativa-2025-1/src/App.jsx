fad91e0a5e2d381340e27e34ec8bf029ebd9198c
juliaStudent18 <juliasouzacosta81@gmail.com>
2025-02-20 22:37:15 -0300

    Trabalhando no NavBar

--- pw3-somativa-2025-1/src/App.jsx
+++ pw3-somativa-2025-1/src/App.jsx
@@ -1,36 +1,12 @@
 import './App.css'
-
-import BookCard from './components/BookCard'
-
-import capa_livro from './assets/cavernas_aco.jpg'
+import NavBar from './components/layout/NavBar'
 
 function App() {
 
   return (
     <>
       <div>
-        <h1>PW3- WEBAPP- LIVRARIA</h1>
-        <BookCard 
-        titulo = 'As cavernas de Aço'
-        autor = 'Issac Azimov' 
-        imagem = {capa_livro}/>
-
-        {/* <BookCard 
-        titulo = 'Orgulho e Preconceito'
-        autor = 'Jane Austin'
-        imagem = 'Uma imagem vai aparecer' />
-
-        <BookCard 
-        titulo = 'Arte da Guerra'
-        autor = 'Sun Tzu' 
-        imagem = 'Uma imagem vai aparecer'/>
-        
-        <BookCard 
-        titulo = 'Turma da Mônica'
-        autor = 'Maurício de Souza' 
-        imagem = 'Uma imagem vai aparecer'/> */}
-    
-       
+       <NavBar />
       </div>
       
     </>
